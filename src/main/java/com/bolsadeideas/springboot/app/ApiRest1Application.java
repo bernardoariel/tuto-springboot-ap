aebfb481b45d82eaf610da3f1f376757ddcceab7
arielbernardo@hotmail.com <arielbernardo@hotmail.com>
2022-10-30 20:52:53 -0300

    fix

--- src/main/java/com/bolsadeideas/springboot/app/ApiRest1Application.java
+++ src/main/java/com/bolsadeideas/springboot/app/ApiRest1Application.java
@@ -2,8 +2,10 @@ package com.bolsadeideas.springboot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bolsadeideas.springboot.app"})
 public class ApiRest1Application {
 
 	public static void main(String[] args) {
